f1b70709f272186b670430e9c0c8a5011a977ec6
mklimasz <mk.klimaszewski@gmail.com>
2016-03-30 21:30:52 +0200

    Add time logs for each calculation

--- MNUM/Project1/mainScript.m
+++ MNUM/Project1/mainScript.m
@@ -1,13 +1,24 @@
-function [] = mainScript(size)
-  [M1, V1, M2, V2, M3, V3] = exampleGenerator(size);
+function [] = mainScript()
   M = [4 -2 4 -2; 3 1 4 2; 2 4 2 1; 2 -2 4 2];
   V = [8; 7; 10; 2;];
   Test = gaussianEliminationCompletePivoting(M, V);
   display(Test);
-  X1 = gaussianEliminationCompletePivoting(M1, V1);
-  display(X1);
-  X2 = gaussianEliminationCompletePivoting(M2, V2);
-  display(X2);
-  X3 = gaussianEliminationCompletePivoting(M3, V3);
-  display(X3);
+  time = 0;
+  size = 10;
+  while size < 50
+    [M1, V1, M2, V2, M3, V3] = exampleGenerator(size);
+    tic;
+    X1 = gaussianEliminationCompletePivoting(M1, V1);
+    printf("Time for data 1 size %d:", size);
+    display(toc);
+    tic;
+    X2 = gaussianEliminationCompletePivoting(M2, V2);
+    printf("Time for data 2 size %d:", size);
+    display(toc);
+    tic;
+    X3 = gaussianEliminationCompletePivoting(M3, V3);
+    printf("Time for data 3 size %d:", size);
+    display(toc);
+    size += 10;
+  end
  end
