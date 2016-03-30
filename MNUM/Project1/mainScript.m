871e20da89da17eb9c047c9e471526b7c9aa3f11
mklimasz <mk.klimaszewski@gmail.com>
2016-03-30 23:20:52 +0200

    Refactoring and removed test example

--- MNUM/Project1/mainScript.m
+++ MNUM/Project1/mainScript.m
@@ -1,12 +1,8 @@
 function [] = mainScript()
-  M = [4 -2 4 -2; 3 1 4 2; 2 4 2 1; 2 -2 4 2];
-  V = [8; 7; 10; 2;];
-  Test = gaussianEliminationCompletePivoting(M, V);
-  display(Test);
-  time = 0;
-  size = 50;
+  start = 10;
+  size = 200;
   fprintf("DATA 1\n");
-  for i = 10:10:size
+  for i = start:3*start:size
     fprintf("\n");
     fprintf("Size %d\n", i);
     [A1, b1] = exampleGenerator(i, 1);
@@ -17,7 +13,7 @@ function [] = mainScript()
     fprintf("Error as residuum norm %d\n", calculateNorm(residuum));
   end
   fprintf("\nDATA 2\n");
-  for i = 10:10:size
+  for i = start:3*start:size
     fprintf("\n");
     fprintf("Size %d\n", i);
     [A2, b2] = exampleGenerator(i, 2);  
@@ -28,7 +24,7 @@ function [] = mainScript()
     fprintf("Error as residuum norm %d\n", calculateNorm(residuum));
   end
   fprintf("\nDATA 3\n");
-  for i = 10:10:size
+  for i = start:3*start:size
     fprintf("\n");
     fprintf("Size %d\n", i);
     [A3, b3] = exampleGenerator(i, 3);    
