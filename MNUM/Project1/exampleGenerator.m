00b5a78db73997e044cffbbc402776a88b9ad264
mklimasz <mk.klimaszewski@gmail.com>
2016-03-30 09:36:34 +0200

    Changed b vectors position in generator

--- MNUM/Project1/exampleGenerator.m
+++ MNUM/Project1/exampleGenerator.m
@@ -10,9 +10,9 @@ function [M1, V1, M2, V2, M3, V3] = exampleGenerator(matrixSize)
         M1(i,j) = 0;
     end
   end
-  V1 = zeros(1, matrixSize);
+  V1 = zeros(matrixSize, 1);
   for i = 1:matrixSize
-    V1(1,i) = 3.4 + 0.6 * i;
+    V1(i, 1) = 3.4 + 0.6 * i;
   end
   M2 = zeros(matrixSize, matrixSize);
   for i = 1:matrixSize
@@ -24,9 +24,9 @@ function [M1, V1, M2, V2, M3, V3] = exampleGenerator(matrixSize)
       end  
     end
   end
-  V2 = zeros(1, matrixSize);
+  V2 = zeros(matrixSize, 1);
   for i = 1:matrixSize
-    V2(1,i) = 8+0.2*i;
+    V2(i,1) = 8+0.2*i;
   end  
   M3 = zeros(matrixSize, matrixSize);
   for i = 1:matrixSize
@@ -34,8 +34,8 @@ function [M1, V1, M2, V2, M3, V3] = exampleGenerator(matrixSize)
       M3(i,j) = 1/[4*(i+j+1)];
     end
   end
-  V3 = zeros(1, matrixSize);
+  V3 = zeros(matrixSize, 1);
   for i = 1:matrixSize
-    V3(1,i) = 2+2/(i+1);
+    V3(i,1) = 2+2/(i+1);
   end
 end
--- MNUM/Project1/mainScript.m
+++ MNUM/Project1/mainScript.m
@@ -1,14 +1,13 @@
-function [Test, Result] = mainScript(size)
+function [] = mainScript(size)
   [M1, V1, M2, V2, M3, V3] = exampleGenerator(size);
   M = [4 -2 4 -2; 3 1 4 2; 2 4 2 1; 2 -2 4 2];
   V = [8; 7; 10; 2;];
-  [Result] = gaussianEliminationCompletePivoting(M, V);
-  display("With Pivoting");
-  display(Result);
-  [Test2, Result2] = gaussianElimination(M,V);
-  display("CORRECT");
-  display(Result2);
-  %X1 = gaussianElimination(M1, V1);
-  %X2 = gaussianElimination(M2, V2);
-  %X3 = gaussianElimination(M3, V3);
+  Test = gaussianEliminationCompletePivoting(M, V);
+  display(Test);
+  X1 = gaussianEliminationCompletePivoting(M1, V1);
+  display(X1);
+  X2 = gaussianEliminationCompletePivoting(M2, V2);
+  display(X2);
+  X3 = gaussianEliminationCompletePivoting(M3, V3);
+  display(X3);
  end
