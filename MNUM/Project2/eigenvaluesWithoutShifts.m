5194b2ff09ee195e6cd78a8b0d102733602a0523
mklimasz <mk.klimaszewski@gmail.com>
2016-04-23 19:29:26 +0200

    Add tolerance parameter for calculating eigenvalues without shifts

--- MNUM/Project2/eigenvaluesWithoutShifts.m
+++ MNUM/Project2/eigenvaluesWithoutShifts.m
@@ -1,9 +1,12 @@
-function [eigenvalues] = eigenvaluesWithoutShifts(A, maxIterations)
+function [eigenvalues, iteration] = eigenvaluesWithoutShifts(A, tol, maxIterations)
     iteration = 0;
-    while iteration < maxIterations
+    while iteration < maxIterations && max(max(A-diag(diag(D)))) > tol
         [Q, R] = qrGramSchmidt(A);
         A = R * Q;
         iteration = iteration + 1;
+    end
+    if iteration > maxIterations
+        error('More iterations needed');
     end
     eigenvalues = diag(A);
 end
